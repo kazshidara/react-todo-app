874d86fc9450423722a323280dbca662c3481b16
Kazune Shidara <kazshidara@ucdavis.edu>
2019-11-29 14:36:00 -0800

    Todo app started, implemented state to display items and toggle feature to cross out item if checked off

---
+++ my-app/src/components/TodoItem.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+
+class TodoItem extends Component {
+
+    getStyle = () => {
+        return {
+            background: '#f4f4f4',
+            padding: '10px',
+            borderBottom: '1px #ccc dotted',
+            textDecoration: this.props.todo.completed ? 'line-through': 'none'
+        }
+    }
+
+    markComplete = (event) => {
+        console.log(this.props)
+    }
+
+    render() {
+        
+        const { id, title} = this.props.todo;
+        return (
+          <div style={ this.getStyle() }>
+              <p>
+                <input type="checkbox" onChange={this.props.markComplete.bind(this,id)} /> {' '}
+                {title}
+              </p>
+          </div>
+      );
+
+    }
+}
+
+// PropTypes
+TodoItem.propTypes = {
+    todo: PropTypes.object.isRequired,
+}
+
+
+
+export default TodoItem;
