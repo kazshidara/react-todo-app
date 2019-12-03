fadd443a7ef1ca154abf36fb422ad4f9de8b1636
Kazune Shidara <kazshidara@ucdavis.edu>
2019-12-03 12:23:36 -0800

    Add new Todo items to list, installed uuid to generate random id numbers so no overlap

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,27 @@
 import React, {Component} from 'react';
 import './App.css';
 import Todos from './components/Todos';
+import Header from './components/layout/header';
+import AddTodo from './components/AddTodo';
+import uuid from 'uuid';
+
 
 class App extends Component {
 
   state = {
     todos: [
       {
-        id: 1,
+        id: uuid.v4(),
         title: 'Take out the trash',
         completed: false
       },
       {
-        id: 2,
+        id: uuid.v4(),
         title: 'Dinner with friends',
         completed: false
       },
       {
-        id: 3,
+        id: uuid.v4(),
         title: 'Meeting with boss',
         completed: false
       }
@@ -39,10 +43,24 @@ class App extends Component {
     this.setState({todos:[...this.state.todos.filter(todo => todo.id !==id)]});
   }
 
+  // Add Todo item to List by using setState and change it via spread operator 
+  addTodo = (title) => {
+    const newTodo = {
+      id: uuid.v4(),
+      title: title,
+      completed:false
+    }
+    this.setState({todos:[...this.state.todos,newTodo]})
+  }
+
   render () {
     return (
-    <div className="App">
-      <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
+     <div className="App">
+       <div className='container'>
+          <Header />
+          <AddTodo addTodo={this.addTodo}/>
+          <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
+      </div>
     </div>
     );
   }  
